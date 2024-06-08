2a1a60dcd667be3c28d83178f475d2376ff560b2
Md. Imran Mir <imranmir6677@gmail.com>
2024-06-09 02:25:39 +0600

    Working on login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import React, { lazy, Suspense } from "react";
 import Loading from "./components/Loading";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
 
 const Home = lazy(() => import("./routes/Home"));
 
@@ -13,6 +15,8 @@ function App() {
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           {/* <Route path="*" element={<Error />} /> */}
         </Routes>
       </Suspense>
