974f409adbc833a3dc629b49b6f5a38bd00b77e2
Md. Imran Mir <imranmir6677@gmail.com>
2024-06-10 00:31:52 +0600

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
 import "./styles/app.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import React, { lazy, Suspense } from "react";
+
 import Loading from "./components/Loading";
 import Login from "./routes/Login";
 import Register from "./routes/Register";
+import ErrorPage from "./error.page";
+
+import { GlobalStateProvider } from "./Context/Global_Context";
+import { GlobalMethodsProvider } from "./Context/GlobalMethodsContext";
 
 const Home = lazy(() => import("./routes/Home"));
+const Profile = lazy(() => import("./routes/Profile"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
-    <Router>
-      <Toaster />
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {/* <Route path="*" element={<Error />} /> */}
-        </Routes>
-      </Suspense>
-    </Router>
+    <GlobalStateProvider>
+      <GlobalMethodsProvider>
+        <Toaster />
+        <Suspense fallback={<Loading />}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </GlobalMethodsProvider>
+    </GlobalStateProvider>
   );
 }
 
